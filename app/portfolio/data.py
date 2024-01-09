6f5ec4e1f22953acb7a1349a0773ddbbb937e9a5
macbook <macbook@macbooks-MacBook-Air.local>
2024-01-09 21:57:55 +0200

    Initial commit

---
+++ app/portfolio/data.py
@@ -0,0 +1,22 @@
+skills = [
+    {
+        'Name': 'Python',
+        'Level': '100',
+    },
+    {
+        'Name': 'Rust',
+        'Level': '50',
+    },
+    {
+        'Name': 'React',
+        'Level': '30',
+    },
+    {
+        'Name': 'JS',
+        'Level': '15',
+    },
+    {
+        'Name': 'Rest',
+        'Level': '4',
+    },
+]
